2222ceb8af05d9ff153c147dc6414058150dea26
İlhan Yiğitoğlu <88687386+Andunie@users.noreply.github.com>
2025-09-14 14:29:26 +0300

    Delete Frontend directory

--- Frontend/src/pages/AdminDashboard.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-export default function AdminDashboard() {
-  return (
-    <div>
-      <h2>Admin Paneli</h2>
-      <p>Görev Durum Dağılımı ve Canlı Grafikler</p>
-    </div>
-  );
-}
